ce4ef2808893955024f4687dbe7cc8d5f92a9829
Fabricio Catae <fcatae@microsoft.com>
2018-09-06 04:29:27 -0300

    contador de acesso em memoria

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,23 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
     public class SimpleBotUser
     {
+        static Dictionary<string, UserProfile> _dictProfiles = new Dictionary<string, UserProfile>();
+
         public static string Reply(Message message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            var id = message.Id;
+
+            var profile = GetProfile(id);
+
+            profile.Visitas += 1;
+
+            SetProfile(id, profile);
+
+            return $"{message.User} disse '{message.Text} e mandou {profile.Visitas} mensagens'";
+        }
+
+        public static void SalvarHistorico(Message message)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var doc = new BsonDocument
+            {
+                { "id", message.Id },
+                { "texto", message.Text},
+                { "app", "teste"}
+            };
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+            col.InsertOne(doc);
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            _dictProfiles.TryGetValue(id, out var profile);
+
+            if( profile == null )
+            {
+                return new UserProfile()
+                {
+                    Id = id, Visitas = 0
+                };
+            }
+
+            return profile;
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            _dictProfiles[id] = profile;
         }
     }
 }

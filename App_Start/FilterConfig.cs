b82c95520499a810b11b96432cf10951e57879fb
lordvelkain <lordvelkain@gmail.com>
2018-09-05 21:59:31 -0300

    Teste com Insert

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBot
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
@@ -9,6 +11,29 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
+            var client = new MongoClient();
+
+            var db = client.GetDatabase("Mongo");
+
+            var col = db.GetCollection<BsonDocument>("tabela1");
+
+            var doc = new BsonDocument
+            {
+                { "id", message.Id },
+                { "texto", message.Text },
+                { "app", "teste" }
+            };
+
+
+            col.InsertOne(doc);
+
+            var colFind = db.GetCollection<BsonDocument>("tabela1");
+
+            
+
+
+
+
             return $"{message.User} disse '{message.Text}'";
         }
 

f55a2cab2a91afae0e755f907aa6cf53b8e4b947
Fabricio Catae <fcatae@microsoft.com>
2018-09-10 20:11:36 -0300

    add repo

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,63 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using MongoDB;
-using MongoDB.Bson;
-using MongoDB.Driver;
+using SimpleBot.Logic;
 
 namespace SimpleBot
 {
-    public class SimpleBotUser
+    public static class SimpleBotUser
     {
-        static Dictionary<string, UserProfile> _dictProfiles = new Dictionary<string, UserProfile>();
+        static IUserProfileRepository _userProfile;
+
+        static SimpleBotUser()
+        {
+            _userProfile = new UserProfileMongoRepo("mongodb://127.0.0.1");
+        }
 
         public static string Reply(Message message)
         {
             var id = message.Id;
 
-            var profile = GetProfile(id);
+            var profile = _userProfile.GetProfile(id);
 
             profile.Visitas += 1;
 
-            SetProfile(id, profile);
+            _userProfile.SetProfile(id, profile);
 
             return $"{message.User} disse '{message.Text} e mandou {profile.Visitas} mensagens'";
         }
 
-        public static void SalvarHistorico(Message message)
-        {
-            var client = new MongoClient("mongodb://localhost:27017");
+        //public static void SalvarHistorico(Message message)
+        //{
+        //    var client = new MongoClient("mongodb://localhost:27017");
 
-            var doc = new BsonDocument
-            {
-                { "id", message.Id },
-                { "texto", message.Text},
-                { "app", "teste"}
-            };
+        //    var doc = new BsonDocument
+        //    {
+        //        { "id", message.Id },
+        //        { "texto", message.Text},
+        //        { "app", "teste"}
+        //    };
 
-            var db = client.GetDatabase("db01");
-            var col = db.GetCollection<BsonDocument>("tabela01");
-            col.InsertOne(doc);
-        }
+        //    var db = client.GetDatabase("db01");
+        //    var col = db.GetCollection<BsonDocument>("tabela01");
+        //    col.InsertOne(doc);
+        //}
 
-        public static UserProfile GetProfile(string id)
-        {
-            _dictProfiles.TryGetValue(id, out var profile);
-
-            if( profile == null )
-            {
-                return new UserProfile()
-                {
-                    Id = id, Visitas = 0
-                };
-            }
-
-            return profile;
-        }
-
-        public static void SetProfile(string id, UserProfile profile)
-        {
-            _dictProfiles[id] = profile;
-        }
     }
 }
